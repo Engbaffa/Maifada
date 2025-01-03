308bbbf2e356f516a4a712c45846e935a4449444
Baffa <iambaffa@gmail.com>
2025-01-03 06:09:15 -0800

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import superRouter from "./routes/super.js";
+import cors from "cors";
 const PORT = 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/s", superRouter);
